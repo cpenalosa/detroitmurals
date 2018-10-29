24a92e5c329cfef311460d5180d0113daeaae5b1
Crystal <penalosa619@gmail.com>
2018-10-29 18:03:38 -0400

    Create murals-importer.js
    comments on vandelay functions

---
+++ murals-importer.js
@@ -0,0 +1,24 @@
+// Imports the data set: https://drive.google.com/open?id=1i2g6lBYBam7-EdAVf2JzBE0mZs029oCiKfEFzTaDQmE
+const formatDate = (fromApi) => util.time.tz(fromApi, 'America/Detroit').toDate() //Formats date and time to Detroit's time zone
+
+export default async (row) => {
+//The geolocation utility allows you to return latitude and longitude if the location is stored as an address. 
+  const location = await util.geo.locate({
+    address: row.Address,
+    city: 'detroit',
+    region: 'michigan'
+  })
+  if (!location) return null //Return a null value if the address field is blank
+  return {
+    id: String(row.id),  
+    artists: row.Artists ? String(row.Artists).split(',').map(util.normalize) : undefined, //Trims, lowercases, and removes redundant whitespace from the given value.
+    startedAt: formatDate(row.Started),
+    name: util.normalize(row.Name),
+    images: row.images ? [
+      row.images
+    ] : undefined,
+    notes: row.Notes ? String(row.Notes) : null,
+    address: row.address,
+    location
+  }
+}
